25b0b309ba8ffbef94ef66823cdcf6067db036fe
Allain Lalonde <allain.lalonde@gmail.com>
2013-11-28 14:36:41 -0500

    Making use of requestAnimationFrame instead of a scheduled activity in the scheduler

--- piccolo2d.js
+++ piccolo2d.js
@@ -798,12 +798,6 @@ var PTransform, PBounds, PPoint, PActivity, PActivityScheduler, PRoot,
       this.root.addChild(this.camera);
       this.camera.addLayer(layer);
 
-      this.camera.getRoot().scheduler.schedule(new PActivity({
-        step: function () {
-          _pCanvas.paint();
-        }
-      }));
-
       function dispatchEvent(type, event, pickedNodes) {
         var currentNode, listener;
 
@@ -1863,11 +1857,11 @@ var PTransform, PBounds, PPoint, PActivity, PActivityScheduler, PRoot,
       this.root.addChild(this.camera);
       this.camera.addLayer(layer);
 
-      this.camera.getRoot().scheduler.schedule(new PActivity({
-        step: function () {
-          _pCanvas.paint();
-        }
-      }));
+      function animate() {
+        requestAnimationFrame(animate);
+        _pCanvas.paint();
+      }
+      setTimeout(animate, 0);
 
       function dispatchEvent(type, event, pickedNodes) {
         var currentNode, listener;
@@ -2128,3 +2122,38 @@ var PTransform, PBounds, PPoint, PActivity, PActivityScheduler, PRoot,
     }
   });
 })();
+
+
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+
+// MIT license
+
+(function () {
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
+      || window[vendors[x] + 'CancelRequestAnimationFrame'];
+  }
+
+  if (!window.requestAnimationFrame)
+    window.requestAnimationFrame = function (callback, element) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function () {
+          callback(currTime + timeToCall);
+        },
+        timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+
+  if (!window.cancelAnimationFrame)
+    window.cancelAnimationFrame = function (id) {
+      clearTimeout(id);
+    };
+}());
