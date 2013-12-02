fa7c142492a02c226062e0ce3dfb5378bedbe2dd
Allain Lalonde <allain.lalonde@gmail.com>
2013-12-02 16:32:21 -0500

    Cleaning up license banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,8 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    banner: grunt.file.read('piccolo2d-license.txt'),
+
     // Task configuration.
     concat: {
       options: {
--- dist/piccolo2d.js
+++ dist/piccolo2d.js
@@ -1,6 +1,31 @@
-/*! piccolo2d - v0.1.0 - 2013-12-01
-* https://github.com/allain/piccolo2d.js
-* Copyright (c) 2013 ; Licensed  */
+/**
+ * Copyright (c) 2008-2013, Piccolo2D project, http://piccolo2d.org
+ * Copyright (c) 1998-2008, University of Maryland
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ * and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * None of the name of the University of Maryland, the name of the Piccolo2D project, or the names of its
+ * contributors may be used to endorse or promote products derived from this software without specific
+ * prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 // From Secrets of a JavaScript ninja
 (function () {
   /* jshint loopfunc: true, noarg: false */
@@ -839,10 +864,12 @@ window.PCanvas = Object.subClass({
       dispatchEvent("mouseover", event, mouseOverNodes);
     }
 
+    var $canvas = jQuery(canvas);
+
     function processMouseEvent(name, event) {
       event.preventDefault();
 
-      var offset = jQuery(canvas).offset(),
+      var offset = $canvas.offset(),
         x = event.pageX - offset.left,
         y = event.pageY - offset.top,
         newPickedNodes = _pCanvas.getPickedNodes(x, y);
@@ -853,8 +880,6 @@ window.PCanvas = Object.subClass({
       previousPickedNodes = newPickedNodes;
     }
 
-    //TODO: Remove  dependence
-    var $canvas = jQuery(canvas);
 
     $canvas.bind("contextmenu", function (event) {
       event.preventDefault();
